d964c92a28ac750eec608bc52de2bf04d4b8383a
Niamul Hasan <48917799+niamul64@users.noreply.github.com>
2021-01-05 04:55:30 +0600

    all done

--- NewsBee/accounts/forms.py
+++ NewsBee/accounts/forms.py
@@ -5,6 +5,26 @@ from .models import ExtentionUser
 
 
 class UserReg(UserCreationForm):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+    email = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+    password1 = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+    password2 = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
 
     class Meta:
         model = User
@@ -12,6 +32,71 @@ class UserReg(UserCreationForm):
 
 
 class ExtentUser(forms.ModelForm):
+    fullName = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'form-control',
+        }
+    ))
+
+    select = (
+        ('', 'Select'),
+        ('ar', 'Argentina'),
+        ('au', 'Australia'),
+        ('at', 'Austria'),
+        ('be', 'Belgium'),
+        ('br', 'Brazil'),
+        ('bg', 'Bulgaria'),
+        ('ca', 'Canada'),
+        ('cn', 'China'),
+        ('co', 'Colombia'),
+        ('cz', 'Czech Republic'),
+        ('eg', 'Egypt'),
+        ('fr', 'France'),
+        ('de', 'Germany'),
+
+        ('gr', 'Greece'),
+        ('hk', 'Hong Kong'),
+        ('hu', 'Hungary'),
+        ('in', 'India'),
+        ('id', 'Indonesia'),
+        ('Ie', 'Ireland'),
+        ('il', 'Israel'),
+        ('it', 'Italy'),
+        ('jp', 'Japan'),
+        ('lv', 'Latvia'),
+        ('lt', 'Lithuania'),
+        ('my', 'Malaysia'),
+        ('mx', 'Mexico'),
+        ('ma', 'Morocco'),
+        ('nl', 'Netherlands'),
+        ('nz', 'New Zealand'),
+        ('ng', 'Nigeria'),
+        ('no', 'Norway'),
+        ('ph', 'Philippines'),
+
+        ('pl', 'Poland'),
+        ('pt', 'Portugal'),
+        ('ro', 'Hungary'),
+        ('sa', 'Saudi Arabia'),
+        ('rs', 'Serbia'),
+        ('sg', 'Singapore'),
+        ('sk', 'Slovakia'),
+        ('si', 'Slovenia'),
+        ('za', 'South Africa'),
+        ('kr', 'South Korea'),
+        ('se', 'Sweden'),
+        ('ch', 'Switzerland'),
+        ('tw', 'Taiwan'),
+        ('th', 'Thailand'),
+        ('tr', 'Turkey'),
+
+        ('ae', 'UAE'),
+        ('ua', 'Ukraine'),
+        ('gb', 'United Kingdom'),
+        ('us', 'United States'),
+        ('ve', 'Venuzuela'),
+
+    )
 
     class Meta:
         model = ExtentionUser
